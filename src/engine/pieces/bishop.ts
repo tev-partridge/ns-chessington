95255c557f1c7e7b596c24902dcaec9f56febb6e
Tev Partridge <Tev.Partridge@softwire.com>
2026-09-10 15:40:53 +0100

    Bishop sliding moves

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,13 +1,39 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import GameSettings from "../gameSettings";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
-    public getAvailableMoves(board: Board) {
-        return new Array(0);
+    public getAvailableMoves(board: Board): Square[] {
+        const fromSquare: Square = board.findPiece(this);
+
+        const destSquares: Square[] = [];
+
+        const fromRow: number = fromSquare.row;
+        const fromCol: number = fromSquare.col;
+
+        // Top left
+        for (let row: number = fromRow + 1, col: number = fromCol - 1; row <= 7 && col >= 0; row++, col--) {
+            destSquares.push(Square.at(row, col));
+        }
+        // Top right
+        for (let row: number = fromRow + 1, col: number = fromCol + 1; row <=7 && col <= 7; row++, col++) {
+            destSquares.push(Square.at(row, col));
+        }
+        // Bottom left
+        for (let row: number = fromRow - 1, col: number = fromCol - 1; row >= 0 && col >= 0; row--, col--) {
+            destSquares.push(Square.at(row, col));
+        }
+        // Bottom right
+        for (let row: number = fromRow - 1, col: number = fromCol + 1; row >= 0 && col <= 7; row--, col++) {
+            destSquares.push(Square.at(row, col));
+        }
+
+        return destSquares;
     }
 }
