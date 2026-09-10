8afdf063a94ac1c3204e74124fb163f11d376477
Tev Partridge <Tev.Partridge@softwire.com>
2026-09-10 12:15:36 +0100

    Pawn single pushes

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,16 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const fromSquare: Square = board.findPiece(this);
+
+        let destSquares: Square[] = [];
+
+        if (this.player === Player.BLACK) {
+            destSquares.push(Square.at(fromSquare.row - 1, fromSquare.col));
+        } else {
+            destSquares.push(Square.at(fromSquare.row + 1, fromSquare.col));
+        }
+
+        return destSquares;
     }
 }
