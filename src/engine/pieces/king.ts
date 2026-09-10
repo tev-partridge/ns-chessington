32a3ac6fe5271f7e5ab88f2da55fb2ab4d8fb3f4
Tev Partridge <Tev.Partridge@softwire.com>
2026-09-10 16:30:12 +0100

    King moves

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,25 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const fromSquare: Square = board.findPiece(this);
+
+        const destSquares: Square[] = [];
+
+        const fromRow: number = fromSquare.row;
+        const fromCol: number = fromSquare.col;
+
+        for (let rowOffset: number = -1; rowOffset <= 1; rowOffset++ ) {
+            for (let colOffset: number = -1; colOffset <= 1; colOffset += 1) {
+                if (colOffset === 0 && rowOffset === 0) continue;
+
+                let destRow: number = fromRow + rowOffset;
+                let destCol: number = fromCol + colOffset;
+
+                if (destRow < 0 || destRow > 7 || destCol < 0 || destCol > 7) continue;
+                destSquares.push(new Square(destRow, destCol));
+            }
+        }
+
+        return destSquares;
     }
 }
