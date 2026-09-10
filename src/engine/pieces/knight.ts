cdce00b75134fa251710e480fb3ed6c551d915b1
Tev Partridge <Tev.Partridge@softwire.com>
2026-09-10 16:07:55 +0100

    Knight moves

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,22 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const fromSquare: Square = board.findPiece(this);
+
+        const fromRow: number = fromSquare.row;
+        const fromCol: number = fromSquare.col;
+
+        let destSquares: Square[] = [];
+
+        for (let rowOffset: number = -2; rowOffset <= 2; rowOffset++) {
+            for (let colOffset: number = -2; colOffset <= 2; colOffset++) {
+                if (Math.abs(rowOffset) + Math.abs(colOffset) !== 3) continue;
+                if (fromRow + rowOffset < 0 || fromRow + rowOffset > 7 || fromCol + colOffset < 0 || fromCol + colOffset > 7) continue;
+
+                destSquares.push(new Square(fromRow + rowOffset, fromCol + colOffset));
+            }
+        }
+
+        return destSquares;
     }
 }
