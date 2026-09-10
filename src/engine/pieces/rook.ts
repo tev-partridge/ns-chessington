8704d72518e519df94f1227a914c3a3b3cda57c6
Tev Partridge <Tev.Partridge@softwire.com>
2026-09-10 14:51:08 +0100

    Implement Rook sliding moves

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,13 +1,31 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import GameSettings from "../gameSettings";
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
-    public getAvailableMoves(board: Board) {
-        return new Array(0);
+    public getAvailableMoves(board: Board): Square[] {
+        const fromSquare: Square = board.findPiece(this);
+
+        const destSquares: Square[] = [];
+
+        const row: number = fromSquare.row;
+        const col: number = fromSquare.col;
+
+        for(let i: number = 0; i < GameSettings.BOARD_SIZE; i++) {
+            if (i === col) continue;
+            destSquares.push(Square.at(row, i));
+        }
+        for(let i: number = 0; i < GameSettings.BOARD_SIZE; i++) {
+            if (i === row) continue;
+            destSquares.push(Square.at(i, col));
+        }
+
+        return destSquares;
     }
 }
